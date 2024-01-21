5187ae309421ee9a1fe7b6d75b4ebc811c666d54
bibamet <spr1tee1337@gmail.com>
2024-01-21 17:52:50 +0300

    21 jan Kalinchuk ADDED UsersMoviesRepoImpl fix

--- src/main/java/repository/impl/UsersMoviesRepoImpl.java
+++ src/main/java/repository/impl/UsersMoviesRepoImpl.java
@@ -2,11 +2,12 @@ package repository.impl;
 
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Root;
 import model.entity.MovieEntity;
 import model.entity.UserEntity;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import repository.UsersMoviesRepo;
 import utils.SQLUtils;
 import utils.SessionFactoryImpl;
@@ -14,35 +15,36 @@ import utils.SessionFactoryImpl;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
 public class UsersMoviesRepoImpl implements UsersMoviesRepo {
-    
+
     private final Connection connection;
     private final SessionFactory sessionFactory = SessionFactoryImpl.getInstance();
-    
+
     public UsersMoviesRepoImpl(Connection connection) {
         this.connection = connection;
     }
-    
-    
+
+
     @Override
     public boolean movieRegistered(MovieEntity movie, Long chatID) {
-        
-        CriteriaBuilder criteriaBuilder = sessionFactory.openSession().getCriteriaBuilder();
-        CriteriaQuery <UserEntity> criteriaQuery = criteriaBuilder.createQuery(UserEntity.class);
-        Root <UserEntity> root = criteriaQuery.from(UserEntity.class);
-        // criteriaQuery.select(root).where(criteriaBuilder.equal(
-        //         root.get("user").
-        // ))
-        
-        Join <MovieEntity, UserEntity> moviesJoin = root.join("users");
-        CriteriaQuery <UserEntity> result = criteriaQuery.select(root).where(criteriaBuilder.and(
-                criteriaBuilder.equal(root.get("chatId"), chatID),
-                criteriaBuilder.equal(moviesJoin.get("id"), movie.getId())
-        ));
-        
-        return false;
-        
+
+        Session session = sessionFactory.openSession();
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<UserEntity> criteriaQuery = criteriaBuilder.createQuery(UserEntity.class);
+        Root<UserEntity> root = criteriaQuery.from(UserEntity.class);
+        root.join("movies");
+        criteriaQuery.select(root).where(criteriaBuilder.and(
+                        criteriaBuilder.equal(root.get("id"), chatID),
+                        criteriaBuilder.equal(root.get("movies").get("id"), movie.getId())
+                )
+        );
+        Query<UserEntity> query = session.createQuery(criteriaQuery);
+        List<UserEntity> resultList = query.getResultList();
+
+        return resultList.size() > 0;
+
         // var result = false;
         // try {
         //     // String query = "insert into movies (id, ref, title, year) values (?, ?, ?, ?)";
@@ -63,25 +65,25 @@ public class UsersMoviesRepoImpl implements UsersMoviesRepo {
         // }
         //  return result;
     }
-    
+
     @Override
     public void deleteMoviesOfUser(Long chatId) {
         try {
-            
+
             String query = """
                     delete from users_movies
                     where user_id = ?
                     """;
-            
+
             PreparedStatement preparedStatement = SQLUtils.getPreparedStatement(query, connection);
             preparedStatement.setLong(1, chatId);
             preparedStatement.execute();
-            
+
             preparedStatement.close();
-            
+
         } catch (SQLException e) {
             throw new RuntimeException("Произошла ошибка выполнения запроса", e);
         }
-        
+
     }
 }
